797b85905153417ab2eba25d342afbc763c531d4
Brent  Ransom <sadumedusa@gmail.com>
2018-07-25 19:47:02 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -10,7 +10,17 @@ class Pokemon
     @@all << self
   end
 
+  def self.all
+    @@all
+  end
+
   def self.save(name, type, database)
     database.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)",name, type)
+  end
+
+  def self.find(num, database)
+    poke = database.execute("select * from pokemon where id = ?", num)
+        # binding.pry
+        self.new(id: poke[0][0], name: poke[0][1], type: poke[0][2], db: database)
   end
 end
